dd7c53583f053f5b2398cc64be8c94cc3b7a77ff
Pål Grønstad Solheim <paul.solheim@gmail.com>
2018-05-22 16:26:19 +0200

    etter 1. gang
    etter 1. kurskveld.

---
+++ Player.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+const SPEED = 500
+
+func _process(delta):
+	if Input.is_action_pressed("left"):
+		position.x -= SPEED * delta
+	elif Input.is_action_pressed("right"):
+		position.x += SPEED * delta
